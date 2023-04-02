462e646264b24e2677353124c133d7feee915aa2
rojasricor <rojasricor@gmail.com>
2023-04-01 23:24:42 -0500

    things improved

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,3 @@
-export const API_DOMAIN = "http://localhost:8080";
-export const API_ROUTE = `${API_DOMAIN}/api`;
+export const API_ROUTE = "http://localhost:8080/api";
 export const RESOURCES_ROUTE = `${API_ROUTE}/get/resources`;
 export const UNSET_STATUS = "unset";

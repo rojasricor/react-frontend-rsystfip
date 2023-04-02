972f06031b848aa83ad635555b9c43e37be20d3f
rojasricor <rojasricor@gmail.com>
2023-04-02 10:53:44 -0500

    pdf running

--- src/components/GetterReports.jsx
+++ src/components/GetterReports.jsx
@@ -4,7 +4,7 @@ import DivCol12 from "./DivCol12";
 import { FaDownload } from "react-icons/fa";
 import { formatTodaysDateTime } from "../utils/resources";
 import pdfMake from "pdfmake/build/pdfmake.min";
-import "pdfmake/build/vfs_fonts";
+import pdfFonts from "pdfmake/build/vfs_fonts";
 
 export default function GetterReports({ startDate, endDate, reportsFiltered }) {
   const [allPeople, setAllPeople] = useState([]);
@@ -52,6 +52,7 @@ export default function GetterReports({ startDate, endDate, reportsFiltered }) {
   }, [startDate, endDate]);
 
   function makePdf() {
+    pdfMake.vfs = pdfFonts.pdfMake.vfs;
     pdfMake
       .createPdf({
         pageMargins: [28, 90],

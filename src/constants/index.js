02a5ad3ef2dcc2357aa4afd12bfe4720cbb88a4c
Ricardo Rojas <rojasricor@gmail.com>
2023-06-13 17:32:20 -0500

    api web added

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_ROUTE = "https://rsystfip.000webhostapp.com/api";
+export const API_ROUTE = "http://localhost/api";
 export const AUTH_KEY = "RSystfip_user_authenticated";
 export const RESOURCE_ROUTE = `${API_ROUTE}/resource`;
 export const UNSET_STATUS = "unset";
